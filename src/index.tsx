197968d5b1979b8d32f6df4a5ba3aa39e27f3062
santerivee <santerivepsa@gmail.com>
2022-05-11 21:11:45 +0300

    fix redirects again

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,6 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
-/*
-    TODO about page
-    credits:
-        {external_link_tag(playlists): "https://commons.wikimedia.org/wiki/File:OOjs_UI_icon_external-link-ltr.svg"}
-*/
-
 const AuthContext = createContext<any>(null);
 
 root.render(<App />);
