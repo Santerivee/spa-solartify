afbc9c54e801fbdf05ecf124f30aebd918be6b5c
santerivee <santerivepsa@gmail.com>
2022-05-12 16:54:39 +0300

    hopefully really really fix small icons

---
+++ src/redirect_uri.js
@@ -0,0 +1,6 @@
+//you might be asking, why does a single string have its own file?
+// well, it's so that i can test it in /test/test.js,
+// without node trying to parse my css and so on
+
+export default "https://solartify.web.app/main"; //prod
+// export default "http://localhost:3000/main"; //dev
---
+++ src/test/test.ts
@@ -0,0 +1,3 @@
+import redirect_uri from "../redirect_uri";
+//@ts-ignore
+if (!(redirect_uri === "https://solartify.web.app/main")) throw new Error("redirect uri is not correct");
