62e14b3c9b50740f14beaf324ead00a38e9844aa
santerivee <santerivepsa@gmail.com>
2022-06-28 21:41:23 +0300

    fix paused UI, add better errror handlnig

--- src/Components/Main/Spinner.tsx
+++ src/Components/Main/Spinner.tsx
@@ -1,10 +1,11 @@
 import "../../styles/Spinner.css";
+import { ErrorModal } from "./ErrorModal";
 
-export const Spinner = ({ text, percentage }: { text: string; percentage?: number }) => {
+export const Spinner = ({ text, percentage, error, setError }: { text: string; percentage?: number; error?: Error | string | null; setError?: any }) => {
     return (
         <div id="spinner-container">
+            {error ? <ErrorModal error={error} setError={setError} /> : null}
             <p id="spinner-text">{text}</p>
-
             <div id="spinner">
                 <p title="this text is not moving" id="spinner-percentage">
                     {percentage ? percentage : 0}%
